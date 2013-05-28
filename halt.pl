ae8f30c03d35382c7711c64e477e74750b46d9f7
Daniel Lyons <dlyons@nrao.edu>
2013-05-28 09:13:26 -0600

    it works

--- halt.pl
+++ halt.pl
@@ -1,9 +1,12 @@
 :- module(halt, [main/0]).
 :- use_module(library(optparse)).
+:- use_module([halt_parser, halt_assembler]).
 
 main :-
     opt_arguments([], _, [Arg]),
     process_file(Arg).
 
-process_file(Arg) :-
-    write('Reading '), write(Arg), nl.
+process_file(File) :-
+    halt_parser:parse_file(File, Code),
+    halt_assembler:execute(Code).
+
