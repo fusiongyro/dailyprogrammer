f8d5312bc271ce3e8c8b651060e62de59128acda
Daniel Lyons <fusion@storytotell.org>
2013-05-28 09:13:26 -0600

    it works

--- halt_machine.pl
+++ halt_machine.pl
@@ -1,16 +1,23 @@
-:- module(halt_machine).
+:- module(halt_machine, [initialize/2,
+                         is_halted/1,
+                         advance/2,
+                         instruction_count/2,
+                         set_register/4,
+                         set_instruction_pointer/3,
+                         set_halted/2]).
 
-:- op(700, xfy, :=).
+initialize(Code, machine(Code, 0, Regs, 0, false)) :- length(Regs, 32), all_zeroes(Regs).
 
-initialize(machine(0, Regs, 0, false)) :- length(Regs, 32), all_zeroes(Regs).
+advance(machine(Code, IC, Regs, IP0, Halted),
+        machine(Code, IC, Regs, IP1, Halted)) :- succ(IP0, IP1).
 
 all_zeroes([0|Rest]) :- all_zeroes(Rest).
 all_zeroes([]).
 
-set_register(machine(InstCount0, Registers0, IP0, Halted),
+set_register(machine(Code, InstCount0, Registers0, IP0, Halted),
 	     Register,
 	     Value,
-	     machine(InstCount1, Registers1, IP1, Halted)) :-
+	     machine(Code, InstCount1, Registers1, IP1, Halted)) :-
     succ(InstCount0, InstCount1),
     succ(IP0, IP1),
     set_register(Register, Value, Registers0, Registers1).
@@ -19,43 +26,22 @@ set_register(N, Value, [X|Rest], [X|Remaining]) :-
     succ(N0, N),
     set_register(N0, Value, Rest, Remaining).
 
-get_register(machine(_, Registers, _, _), Register, Value) :-
+get_register(machine(_, _, Registers, _, _), Register, Value) :-
     nth0(Register, Registers, Value).
 
-set_instruction_pointer(machine(IC, Instructions, _, Halted),
+set_instruction_pointer(machine(Code, IC0, Instructions, _, Halted),
 			X,
-			machine(IC, Instructions, X, Halted)).
+			machine(Code, IC1, Instructions, X, Halted)) :-
+    succ(IC0, IC1).
 
-set_halted(machine(IC0, Code, IP0, _),
-	   machine(IC1, Code, IP1, true)) :-
+is_halted(machine(_, _, _, _, true)).
+
+instruction_count(machine(_, IC, _, _, _), IC).
+
+set_halted(machine(Code, IC0, Reg, IP0, _),
+	       machine(Code, IC1, Reg, IP1, true)) :-
     succ(IC0, IC1),
     succ(IP0, IP1).
 
-%% The table of operations
-spec(and(A,B))          :- m(A) := m(A) \/ m(B).
-spec(or(A,B))           :- m(A) := m(A) /\  m(B).
-spec(exclusive_or(A,B)) :- m(A) := m(A) xor m(B).
-spec(not(A))            :- m(A) := \ m(A).
-spec(move(A,B))         :- m(A) := m(B).
-spec(set(A,C))          :- m(A) := C.
-spec(random(A))         :- m(A) := random.
-spec(jump(X))           :- ip   := X.
-spec(jump_if_zero(X,A)) :- m(A) -> ip := X.
-spec(halt)              :- halted := true.
-
-%% Basic operations
-evaluate(M, m(A), V) :- get_register(M, A, V).
-evaluate(M, A/\B, V) :- evaluate(M, A, RA), evaluate(M, B, RB), V is RA /\ RB.
-evaluate(M, A\/B, V) :- evaluate(M, A, RA), evaluate(M, B, RB), V is RA \/ RB.
-evaluate(M, A xor B, V) :- evaluate(M, A, RA), evaluate(M, B, RB), V is RA xor RB.
-evaluate(M, \ A, V) :- evaluate(M, A, RA), V is \ RA.
-evaluate(M, A -> B, V) :- evaluate(M, A, RA), RA -> evaluate(M, B, V).
-evaluate(M, m(A) := B, V) :- evaluate(M, B, RB), set_register(M, A, RB, V).
-evaluate(_, A, A) :- number(A).
-evaluate(M, ip := X, MA) :- set_instruction_pointer(M, X, MA).
-evaluate(M, halted := true, MA) :- set_halted(M, MA).
-evaluate(_, random, V) :- V is random(2).
-
-%% Perform the specification
-evaluate(M, X, V) :-
-    clause(spec(X), Body), evaluate(M, Body, V).
+next_instruction(machine(Code, _, _, IP, _), Instruction) :-
+    nth0(IP, Code, Instruction).
