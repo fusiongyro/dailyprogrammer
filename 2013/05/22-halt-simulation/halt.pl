0722705a9789edaf65e210855f4cadaa67da0106
Daniel Lyons <dlyons@nrao.edu>
2013-05-28 16:52:43 -0600

    fixing pldoc comments

--- 2013/05/22-halt-simulation/halt.pl
+++ 2013/05/22-halt-simulation/halt.pl
@@ -2,16 +2,19 @@
 :- use_module(library(optparse)).
 :- use_module([halt_parser, halt_assembler]).
 
-%% Main entry point: run a single argument.
+/** main is det.
+ * 
+ * Main entry point: run a single argument.
+ */
 main :-
     opt_arguments([], _, [Arg]),
     process_file(Arg).
 
-%! process_file(+Filename) is semidet.
-%%
-%%    Attempt to execute the machine using the code in the specified
-%%    file.
+/** process_file(+Filename) is semidet.
+ * 
+ * Attempt to execute the machine using the code in the specified
+ * file.
+ */
 process_file(File) :-
     halt_parser:parse_file(File, Code),
     halt_assembler:execute(Code).
-
--- 2013/05/22-halt-simulation/halt_assembler.pl
+++ 2013/05/22-halt-simulation/halt_assembler.pl
@@ -3,11 +3,15 @@
 
 :- op(700, xfy, :=).
 
-%! <module> Assembler
-%%
-%% Given a sequence of machine instructions, perform them on the raw machine.
+/** <module> Assembler
+ *
+ * Given a sequence of machine instructions, perform them on the raw machine.
+ *
+ * @author Daniel Lyons
+ * @license MIT
+ */
 
-%% The table of operations
+% The table of operations
 spec(and(A,B))          :- m(A) := m(A) \/ m(B).
 spec(or(A,B))           :- m(A) := m(A) /\  m(B).
 spec(exclusive_or(A,B)) :- m(A) := m(A) xor m(B).
@@ -19,8 +23,8 @@ spec(jump(X))           :- ip   := X.
 spec(jump_if_zero(X,A)) :- (m(A) = 0) -> ip := X ; advance.
 spec(halt)              :- halted := true.
 
-%% Basic operations
-evaluate(M, m(A),        V) :- halt_machine:get_register(M, A, V).
+% Basic operations
+evaluate(M, m(A),        V) :- get_register(M, A, V).
 evaluate(M, A /\ B,      V) :- evaluate(M, A, RA), evaluate(M, B, RB),
                                V is RA /\ RB.
 evaluate(M, A \/ B,      V) :- evaluate(M, A, RA), evaluate(M, B, RB),
@@ -37,55 +41,59 @@ evaluate(M, advance,     V) :- advance(M, V).
 evaluate(M, A = B,       V) :- evaluate(M, A, RA), evaluate(M, B, RB),
                                RA = RB -> V = true ; V = false.
 
-%% Special cases
+% Special cases
 evaluate(M, ip := X,        MA) :- set_instruction_pointer(M, X, MA).
 evaluate(M, halted := true, MA) :- set_halted(M, MA).
 
-%% Perform the specification
+% Perform the specification
 evaluate(M, X, V) :-
     clause(spec(X), Body), evaluate(M, Body, V).
 
-%! execute(+Code)
-%%
-%%   Executes the sequence of assembler instructions, printing either
-%%   'Program halts!' if the machine halts or runs out of
-%%   instructions, otherwise printing 'Unable to determine if
-%%   application halts' if we hit the 10,000th instruction without
-%%   halting.
+/** execute(+Code:[instruction]) is det.
+ *
+ * Executes the sequence of assembler instructions, printing either
+ * 'Program halts!' if the machine halts or runs out of
+ * instructions, otherwise printing 'Unable to determine if
+ * application halts' if we hit the 10,000th instruction without
+ * halting.
+ */
 execute(Code) :- execute(Code, _).
 
-%! execute(+Code, -FinalMachine)
-%%
-%%   Execute Code as a sequence of assembler instructions, unifying
-%%   the final state of the machine with FinalMachine.
+/** execute(+Code, -FinalMachine) is det.
+ *
+ * Execute Code as a sequence of assembler instructions, unifying
+ * the final state of the machine with FinalMachine.
+ */
 execute(Code, FinalMachine) :-
-    halt_machine:initialize(Code, InitialMachine),
+    initialize(Code, InitialMachine),
     execute_loop(InitialMachine, FinalMachine).
-
-%% Executes a single instruction, then determine if we should loop or
-%% not, looping if so.
+
+% Executes a single instruction, then determine if we should loop or
+% not, looping if so.
 execute_loop(Machine0, MachineN) :-
     execute_one(Machine0, Machine1),
 
     %% if we're halted, display the message; otherwise continue
-    ((is_halted(Machine1) ; halt_machine:instruction_count(Machine1, 10000))
+    ((is_halted(Machine1) ; instruction_count(Machine1, 10000))
        -> do_halt(Machine1)
         ; execute_loop(Machine1, MachineN)).
 
-%! execute_one(+InputMachine, -OutputMachine) is semidet.
-%%
-%%   Executes one instruction.
+/** execute_one(+InputMachine, -OutputMachine) is semidet.
+ *
+ * Executes one instruction.
+ */
 execute_one(Machine0, Machine1) :-
     %% fetch the next instruction
-    halt_machine:next_instruction(Machine0, Inst),
+    next_instruction(Machine0, Inst),
 
     %% evaluate it
     evaluate(Machine0, Inst, Machine1).
 
-%! do_halt(+Machine) is det.
-%%
-%%   Determine whether halt was artificial or natural and write to
-%%   standard output accordingly.
+/** do_halt(+Machine) is det.
+ *
+ * Determine whether halt was artificial or natural and write to
+ * standard output accordingly.
+ */
 do_halt(machine(_, _, _, _, true)) :-
     write('Program halts!'), nl.
 do_halt(machine(Code, _, _, IP, _)) :-
