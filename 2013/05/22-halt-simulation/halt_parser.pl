b7932c70a633eaf5bf2936ab87b6ee0f85224ff1
Daniel Lyons <fusion@storytotell.org>
2013-06-10 16:58:11 -0600

    working on call forwarding

---
+++ 2013/05/22-halt-simulation/halt_parser.pl
@@ -0,0 +1,44 @@
+:- module(halt_parser, [parse/2, parse_file/2]).
+:- use_module(library(dcg/basics)).
+
+/** <module> Parse the assembler code file
+ *
+ * Our intermediate representation is encoded directly in the
+ * instruction patterns below.
+ */
+
+int(X) --> integer(X).
+
+instruction(and(A,B))           --> "AND", whites, int(A), whites, int(B).
+instruction(or(A,B))            --> "OR",  whites, int(A), whites, int(B).
+instruction(exclusive_or(A,B))  --> "XOR", whites, int(A), whites, int(B).
+instruction(not(A))             --> "NOT", whites, int(A).
+instruction(move(A,B))          --> "MOV", whites, int(A), whites, int(B).
+instruction(set(A,C))           --> "SET", whites, int(A), whites, int(C).
+instruction(random(A))          --> "RANDOM", whites, int(A).
+instruction(jump(X))            --> "JMP", whites, int(X).
+instruction(jump_if_zero(X, A)) --> "JZ", whites, int(X), whites, int(A).
+instruction(halt)               --> "HALT".
+
+instructions([Inst|Instructions]) -->
+    instruction(Inst),
+    blanks,
+    !,
+    instructions(Instructions).
+instructions([]) --> [].
+
+program(Program) --> integer(_), blanks, instructions(Program).
+
+/** parse(+Input, -Program) is semidet.
+ *
+ * Parse string Input into a sequence of code statements.
+ */
+parse(Input, Program) :- phrase(program(Program), Input).
+
+/** parse_file(+Filename, -Program) is semidet.
+ *
+ * Parse the file specified by Filename into a sequence of code
+ * statements.
+ */
+parse_file(Filename, Program) :-
+    pio:phrase_from_file(halt_parser:program(Program), Filename).
---
+++ README.md
@@ -0,0 +1,12 @@
+## Daily Programmer
+
+Implementations of things from the /r/dailyprogrammer.
+
+So far I have:
+
+ * [Halt Simulation (intermediate)](https://bitbucket.org/fusiongyro/dailyprogrammer/src/tip/2013/05/22-halt-simulation/) (in Prolog).
+   [Spec on /r/dailyprogrammer](http://www.reddit.com/r/dailyprogrammer/comments/1euacb/052213_challenge_125_intermediate_halt_its/)
+ * [Change Calculator (easy)](https://bitbucket.org/fusiongyro/dailyprogrammer/src/tip/2013/01/28-change-calculator/change.pl) (in Prolog).
+   [Spec on /r/dailyprogrammer](http://www.reddit.com/r/dailyprogrammer/comments/17f3y2/012813_challenge_119_easy_change_calculator/)
+ * [Sum Digits II (easy)](https://bitbucket.org/fusiongyro/dailyprogrammer/src/tip/2013/06/04-sum-digits/sum_digits.pl) (in Prolog).
+   [Spec on /r/dailyprogrammer](http://www.reddit.com/r/dailyprogrammer/comments/1fnutb/06413_challenge_128_easy_sumthedigits_part_ii/)
